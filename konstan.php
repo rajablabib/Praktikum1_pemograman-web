08ced10bee5c576a25b291b5787a79726aba0b6b
Rajab Labib Nasiruddin <91243827+rajablabib@users.noreply.github.com>
2022-03-01 20:49:24 +0700

    Add files via upload

---
+++ konstan.php
@@ -0,0 +1,14 @@
+<?php
+    //menghitung Luas Lingkaran
+    define ( 'phi' , 3.14);
+
+    $jari = 40;
+    $luas = phi*$jari*$jari;
+    $kll = 2*phi*$jari;
+
+    echo 'Luas lingkarang dengan jari-jari' .$jari .'=' .$luas;
+    echo '<br/>dan Keliling lingkaranya adalah =' .$kll;
+?>
+
+
+
---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+    $nama = 'Rajab Labib Nasiruddin';
+    $umur = 18;
+    $prodi = 'sistem informasi';
+
+
+    echo 'Nama : '.$nama;
+    echo '<br/>Umur : '.$umur;
+    echo '<br/>Program studi : '.$prodi;
+
+
+    echo "<br/> Hai $nama Apa Kabar";
+?>
